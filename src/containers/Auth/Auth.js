8c23701b5d37c1224007d883aab65e76e3f13303
Евгений <grigar@gmail.com>
2020-07-05 18:19:27 +0300

    создал форму QuizCreator(добавление вопросос)+ валидация данных на этой форме и записаь новых вопросов в массив.

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -13,6 +13,8 @@ import is from 'is_js'
 export default class Auth extends Component{
 
     state={
+        isFormValid: false,
+
         formControls:{
             email: {
                 value: '',
@@ -81,8 +83,13 @@ export default class Auth extends Component{
 
         formControls[controlName]=control
 
+        let isFormValid = true
+        Object.keys(formControls).forEach(name =>{
+            isFormValid = formControls[name].valid && isFormValid
+        })
+
         this.setState({
-            formControls
+            formControls, isFormValid
         })
     }
 
@@ -132,6 +139,7 @@ export default class Auth extends Component{
                         <Button
                             type="success"
                             onClick={this.loginHandler}
+                            disabled = {!this.state.isFormValid}
                         >
                          Войти
                         </Button>
@@ -139,6 +147,7 @@ export default class Auth extends Component{
                         <Button
                             type="primary"
                             onClick={this.registerHandler}
+                            disabled = {!this.state.isFormValid}
                         >
                             Зарегистрироваться
                         </Button>
